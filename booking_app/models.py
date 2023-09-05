42d441d87dca3552cbe606e0365ceec2fefe382d
iweinacker <>
2023-09-05 10:58:16 +0000

    Create model of cutsomer, table and booking

--- booking_app/models.py
+++ booking_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+from django.utils import timezone
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50, unique=True, blank=True,
+                                  editable=True)
+    last_name = models.CharField(max_length=50, unique=True, blank=True,
+                                 editable=True)
+    phone = models.CharField(max_length=100)
+
+
+class Table(models.Model):
+    seats = models.IntegerField()
+    minimun_people = models.IntegerField()
+    maximum_people = models.IntegerField()
+
+
+class Booking(models.Model):
+    table = models.ForeignKey('Table', on_delete=models.CASCADE)
+    group = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    date_required = models.DateField()
+    date_created = models.DateTimeField(default=timezone.now, blank=True)
+    date_updated = models.DateTimeField(default=timezone.now, blank=True)
+
