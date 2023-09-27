e863d406403b72c48b661f186a59ae1da966efa5
iweinacker <>
2023-09-27 17:04:22 +0000

    adding alert message to the user

--- booking_app/views.py
+++ booking_app/views.py
@@ -4,17 +4,21 @@ from .models import Booking
 from django.contrib.auth.models import User
 from .forms import ReservationForm
 from datetime import datetime, timedelta
+from django.contrib import messages
+from django.http import HttpResponseRedirect, HttpResponse
+
+
+from django.contrib import messages
 
 
 def reservation_view(request):
     if request.method == 'POST':
         form = ReservationForm(request.POST)
         if form.is_valid():
-            # Save the form data to the database
             booking = Booking(
                 first_name=form.cleaned_data['first_name'],
                 last_name=form.cleaned_data['last_name'],
-                email=form.cleaned_data['last_name'],
+                email=form.cleaned_data['email'],
                 phone=form.cleaned_data['phone'],
                 seats=form.cleaned_data['seats'],
                 date_required=form.cleaned_data['date_required'],
@@ -22,7 +26,11 @@ def reservation_view(request):
             )
             booking.save()
 
-            return redirect('success')
+            messages.success(
+                request, 'Booking complete, we are checking our availability, please wait for a call back.')
+        else:
+            messages.error(
+                request, 'There was an error with your submission. Please try again.')
     else:
         form = ReservationForm()
 
