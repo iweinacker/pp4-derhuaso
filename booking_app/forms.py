61bb51db73cef81db01a8d80352435e481b0fd0f
iweinacker <130374663+iweinacker@users.noreply.github.com>
2023-09-28 22:53:28 +0200

    Update forms.py

--- booking_app/forms.py
+++ booking_app/forms.py
@@ -4,26 +4,6 @@ from django.core.exceptions import ValidationError
 from .models import Booking
 from django.utils import timezone
 
-HOURS = (
-    ("6 PM", "6 PM"),
-    ("6:30 PM", "6:30 PM"),
-    ("7 PM", "7 PM"),
-    ("7:30 PM", "7:30 PM"),
-    ("8 PM", "8 PM"),
-    ("8:30 PM", "8:30 PM"),
-    ("9 PM", "9 PM"),
-    ("9:30 PM", "9:30 PM"),
-    ("10 PM", "10 PM"),
-)
-
-SEATS = (
-    ("2", "2"),
-    ("3", "3"),
-    ("4", "4"),
-    ("5", "5"),
-    ("6", "6"),
-)
-
 
 class ReservationForm(forms.ModelForm):
     class Meta:
@@ -40,8 +20,3 @@ class ReservationForm(forms.ModelForm):
             'time': forms.Select(attrs={'class': 'form-control'}),
         }
 
-    # def clean_date_required(self):
-    #     date_required = self.cleaned_data['date_required']
-    #     if date_required and date_required < timezone.localdate():
-    #         raise ValidationError("Date cannot be in the past")
-    #     return date
